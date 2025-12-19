fbb0148bdf51e835fbb293c4959003353eec347b
shaibow.s <alexandrekuoch18@gmail.com>
2025-12-19 01:40:37 +0100

    commm

--- debut.go
+++ debut.go
@@ -1,34 +1,26 @@
 package main
 
-import (
-	// Librairie pour exécuter des requêtes SQL
-	"database/sql"
-	"fmt"
-	"log"
-	"net/http"
+// Librairie pour exécuter des requêtes SQL
 
-	"github.com/aminnairi/go-authentication-demo/routes"
-)
+// func main() {
+// 	database, openError := sql.Open("sqlite", "./database.sqlite")
 
-func main() {
-	database, openError := sql.Open("sqlite", "./database.sqlite")
+// 	if openError != nil {
+// 		log.Fatal("Error while opening the database:", openError)
+// 	}
+// 	initializeDataModel(database)
 
-	if openError != nil {
-		log.Fatal("Error while opening the database:", openError)
-	}
-	initializeDataModel(database)
+// 	// Les routes HTTP
+// 	http.HandleFunc("POST /signup", routes.Signup(database))
 
-	// Les routes HTTP
-	http.HandleFunc("POST /signup", routes.Signup(database))
+// 	fmt.Println("Starting http server on http://localhost:8080...")
 
-	fmt.Println("Starting http server on http://localhost:8080...")
-
-	// On démarre le serveur HTTP et on vérifie si aucune erreur ne survient
-	if listenError := http.ListenAndServe(":8080", nil); listenError != nil {
-		// Si une erreur survient
-		log.Fatal("Error while listening for http requests on http://localhost:8080", listenError)
-	}
-}
+// 	// On démarre le serveur HTTP et on vérifie si aucune erreur ne survient
+// 	if listenError := http.ListenAndServe(":8080", nil); listenError != nil {
+// 		// Si une erreur survient
+// 		log.Fatal("Error while listening for http requests on http://localhost:8080", listenError)
+// 	}
+// }
 
 // //// Début de chaque fonction
 // func Signin(database *sql.DB) http.HandlerFunc {
