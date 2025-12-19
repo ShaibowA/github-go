7e5c601dca7703c0863a5c281d7555e8e3284e3c
shaibow.s <alexandrekuoch18@gmail.com>
2025-12-19 01:29:26 +0100

    com

---
+++ debut.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	// Librairie pour exécuter des requêtes SQL
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/aminnairi/go-authentication-demo/routes"
+)
+
+func main() {
+	database, openError := sql.Open("sqlite", "./database.sqlite")
+
+	if openError != nil {
+		log.Fatal("Error while opening the database:", openError)
+	}
+	initializeDataModel(database)
+
+	// Les routes HTTP
+	http.HandleFunc("POST /signup", routes.Signup(database))
+
+	fmt.Println("Starting http server on http://localhost:8080...")
+
+	// On démarre le serveur HTTP et on vérifie si aucune erreur ne survient
+	if listenError := http.ListenAndServe(":8080", nil); listenError != nil {
+		// Si une erreur survient
+		log.Fatal("Error while listening for http requests on http://localhost:8080", listenError)
+	}
+}
+
+// //// Début de chaque fonction
+// func Signin(database *sql.DB) http.HandlerFunc {
+// 	return func(response http.ResponseWriter, request *http.Request) {
+
+// /// Creation de la table
+// func initializeDataModel(database *sql.DB) {
+//     _, execError := database.Exec(`
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ShaibowA/github-go
+
+go 1.25.4
